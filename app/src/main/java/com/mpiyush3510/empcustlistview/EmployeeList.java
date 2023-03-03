a0087d043f2f38045a67e0bd4d838479c4a63b7b
PIYUSH MAKWANA <mpiyush3510@cbigujarat.onmicrosoft.com>
2023-03-03 21:30:02 +0530

    Unmodified

---
+++ app/src/main/java/com/mpiyush3510/empcustlistview/EmployeeList.java
@@ -0,0 +1,13 @@
+package com.mpiyush3510.empcustlistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmployeeList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_list);
+    }
+}
